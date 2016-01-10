6b905beb0229b10a251a72c4098f4db1b4a96ff2
Brian Kelly <polymonic@gmail.com>
2016-01-10 12:29:59 -0500

    Switch to Slim for layouts

--- config.rb
+++ config.rb
@@ -14,29 +14,9 @@ set :js_dir, 'javascripts'
 
 set :layout, 'tdd'
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-###
-# Helpers
-###
 
 helpers do
   def section(section_name)
@@ -50,7 +30,6 @@ helpers do
   end
 end
 
-# Build-specific configuration
 configure :build do
   # Minify CSS on build
   # activate :minify_css
@@ -58,3 +37,4 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 end
+
