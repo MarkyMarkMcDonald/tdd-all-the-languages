efa1d26c2b58a9184f5e97fe6b7aab843cf50ca9
Brian Kelly <polymonic@gmail.com>
2016-01-17 12:34:21 -0500

    Fake out sitemap generator step until PDFs are actually built

--- makepdf.rb
+++ makepdf.rb
@@ -4,18 +4,30 @@ require 'fileutils'
 class PdfGenerator < Middleman::Extension
   LANGUAGES = %w(ruby java kotlin node python ruby elixir)
 
+  def manipulate_resource_list(resources)
+    FileUtils::mkdir_p 'build/pdfs'
+
+    LANGUAGES.each do |language|
+      # Fake out Sitemap until PDFs are actually built
+      FileUtils.touch("build/pdfs/#{language}.pdf")
+      resources << Middleman::Sitemap::Resource.new(app.sitemap, "pdfs/#{language}.pdf", "build/pdfs/#{language}.pdf")
+    end
+
+    resources
+  end
+
   def after_build(builder)
     FileUtils::mkdir_p 'build/pdfs'
 
     LANGUAGES.each do |language|
       begin
         kit = PDFKit.new(File.new("build/language/#{language}/index.html"),
-                         :margin_top => 10,
-                         :margin_bottom => 10,
-                         :margin_left => 10,
-                         :margin_right => 10,
-                         :print_media_type => true,
-                         :dpi => 96)
+          :margin_top => 10,
+          :margin_bottom => 10,
+          :margin_left => 10,
+          :margin_right => 10,
+          :print_media_type => true,
+          :dpi => 96)
 
         kit.to_file("build/pdfs/#{language}.pdf")
 
@@ -27,16 +39,6 @@ class PdfGenerator < Middleman::Extension
 
     builder.say_status "PDF Maker",  "PDFs Generated!"
   end
-
-  def manipulate_resource_list(resources)
-    LANGUAGES.each do |language|
-      resources << Middleman::Sitemap::Resource.new(app.sitemap, "pdfs/#{language}.pdf", "build/pdfs/#{language}.pdf")
-    end
-
-    resources
-  end
-
 end
-
 
 ::Middleman::Extensions.register(:pdfgenerator, PdfGenerator)
